c5964e9193907f8159418f20bcca45dea804bf76
Adrian Cole <adrian@tetrate.io>
2022-10-05 11:05:58 +0200

    Adds benchmarks vs wat
    This helps us identify where tuning can help regarding how memory is
    used. This should also help assess perf moving between versions.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

---
+++ internal/e2e/benchmark_test.go
@@ -0,0 +1,143 @@
+package e2e_test
+
+import (
+	"context"
+	_ "embed"
+	"net/http"
+	"net/url"
+	"testing"
+
+	nethttp "github.com/http-wasm/http-wasm-host-go/handler/nethttp"
+
+	testdata "github.com/http-wasm/http-wasm-guest-tinygo/internal/test"
+)
+
+var (
+	readOnlyRequest = &http.Request{
+		Method: "GET",
+		URL:    &url.URL{Path: "/v1.0/hi"},
+		Header: http.Header{},
+	}
+
+	readOnlyRequestWithHeader = &http.Request{
+		Method: "GET",
+		URL:    &url.URL{Path: "/v1.0/hi"},
+		Header: http.Header{"Accept": {"text/plain"}},
+	}
+)
+
+var benches = map[string]struct {
+	bins       map[string][]byte
+	newRequest func() *http.Request
+}{
+	"get_path": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchGetPathTinyGo,
+			"wat":    testdata.BinBenchGetPathWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequest
+		}},
+	"get_request_header exists": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchGetRequestHeaderTinyGo,
+			"wat":    testdata.BinBenchGetRequestHeaderWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequestWithHeader
+		}},
+	"get_request_header not exists": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchGetRequestHeaderTinyGo,
+			"wat":    testdata.BinBenchGetRequestHeaderWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequest
+		}},
+	"log": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchLogTinyGo,
+			"wat":    testdata.BinBenchLogWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequest
+		}},
+	"next": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchNextTinyGo,
+			"wat":    testdata.BinBenchNextWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequest
+		}},
+	"sendResponse": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchSendResponseTinyGo,
+			"wat":    testdata.BinBenchSendResponseWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequest
+		}},
+	"setPath": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchSetPathTinyGo,
+			"wat":    testdata.BinBenchSetPathWat,
+		},
+		newRequest: func() *http.Request {
+			return &http.Request{}
+		}},
+	"setResponseHeader": {
+		bins: map[string][]byte{
+			"TinyGo": testdata.BinBenchSetResponseHeaderTinyGo,
+			"wat":    testdata.BinBenchSetResponseHeaderWat,
+		},
+		newRequest: func() *http.Request {
+			return readOnlyRequest
+		}},
+}
+
+func Benchmark(b *testing.B) {
+	for n, s := range benches {
+		b.Run(n, func(b *testing.B) {
+			for n, bin := range s.bins {
+				benchmark(b, n, bin, s.newRequest)
+			}
+		})
+	}
+}
+
+func benchmark(b *testing.B, name string, bin []byte, newRequest func() *http.Request) {
+	ctx := context.Background()
+
+	mw, err := nethttp.NewMiddleware(ctx, bin)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer mw.Close(ctx)
+
+	h := mw.NewHandler(ctx, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+
+	b.Run(name, func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			h.ServeHTTP(fakeResponseWriter{}, newRequest())
+		}
+	})
+}
+
+var _ http.ResponseWriter = fakeResponseWriter{}
+
+type fakeResponseWriter struct{}
+
+func (rw fakeResponseWriter) Header() http.Header {
+	return http.Header{}
+}
+
+func (rw fakeResponseWriter) Write(b []byte) (int, error) {
+	return len(b), nil
+}
+
+func (rw fakeResponseWriter) WriteHeader(int) {
+}
